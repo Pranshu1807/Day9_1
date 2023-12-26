4498139e27f696b3ffda02df3844935d6fa0f074
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-27 05:23:02 +0530

    UC6 done

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@ public class Main {
         L1.add(70);
         L1.insertAtPosition(1, 30);
         L1.display();
-        L1.popLast();
-        L1.display();
+        int find = L1.find(30);
+        if (find == -1)
+            System.out.println("Node not found");
+        else
+            System.out.println("Node found at position " + find);
+
     }
 }
