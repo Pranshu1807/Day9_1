5bd9fd808c07816895d9e7e5d808cb0dff3c4e2b
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-27 04:59:25 +0530

    UC2 done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList L1 = new LinkedList();
+        L1.add(56);
+        L1.display();
+        L1.add(30);
+        L1.display();
         L1.add(70);
-        L1.display();
-        L1.insertAtPosition(0, 30);
-        L1.display();
-        L1.insertAtPosition(0, 56);
         L1.display();
     }
 }
