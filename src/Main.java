b76070eebd5becb8a1f850383e55b8b1bcf197ae
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-27 05:41:23 +0530

    UC8 done

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ public class Main {
         L1.add(56);
         L1.add(70);
         L1.insertAtPosition(1, 30);
-        L1.display();
         L1.insertAfter(40, 30);
         L1.display();
+        System.out.println("The size of Linked List is " + L1.length());
+
+        L1.deleteByValue(40);
+        L1.display();
+        System.out.println("The size of Linked List is " + L1.length());
     }
 }
